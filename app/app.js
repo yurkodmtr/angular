9d1b50df6d588ea1afc1e2e1276b1fbf7a1f1159
yurko <4au.4au.4au@gmail.com>
2017-05-23 19:03:52 +0300

    1qwg

--- app/app.js
+++ app/app.js
@@ -1,1 +1,20 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "app/components/index/index.html",
+            controller : "indexCtrl"
+        })
+        .when("/about", {
+            templateUrl : "app/components/about/about.html",
+            controller : "aboutCtrl"
+        });
+});
+
+/* active link menu */
+app.controller('menuCtrl', function($scope, $location) {
+    $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
+});
