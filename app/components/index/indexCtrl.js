7df7cf3528d4369fa254b1bb83fb60cbc6ba742c
yurko_DMTR <4au.4au.4au@gmail.com>
2017-05-29 17:47:56 +0300

    nn

--- app/components/index/indexCtrl.js
+++ app/components/index/indexCtrl.js
@@ -1,5 +1,9 @@
 app.controller("indexCtrl", function($scope, helperFactory) {
 
+    $scope.data = {
+        checkAllCheckboxes: false
+    };
+
     $scope.json1 = [];
     helperFactory.getData('json1.json').then(function(data) {
         $scope.json1 = data.data;
@@ -29,6 +33,26 @@ app.controller("indexCtrl", function($scope, helperFactory) {
         if ( count < 10 ) {
             $('.btn_add_color').attr('disabled', false);
         }
+    }
+
+    $scope.checkAllCheckboxesEvent = function(){
+        angular.forEach($scope.json2.colorsArray, function(value, key) {
+          value.isActive = $scope.data.checkAllCheckboxes;
+        });
+    }
+
+    $scope.removeActiveAll = function(val){
+        $scope.data.checkAllCheckboxes = false;
+    }
+
+    $scope.removeSelected = function(){
+        var i = 0;
+        angular.forEach($scope.json2.colorsArray, function(value, key) {
+            if (value.isActive == true) {
+                $scope.json2.colorsArray.splice(i, 1);
+                i++;
+            }
+        });
     }
 
 });
